83b5b6426616e1cdf0cb6c3bf52add2b21933056
Trollmine <trolltrollmine@gmail.com>
2025-12-20 17:07:51 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
-# Mp3 reader V2:
+# Mp3 reader V3:
 import customtkinter
 from tkinter import filedialog
-import pathlib
 import pygame
 import random
 import os
@@ -16,6 +15,17 @@ songsCount     = 0
 playButton = customtkinter.CTkButton(app, text="PLAY")
 playButton.pack(padx=20, pady=20)
 
+changeDirectoryButton = customtkinter.CTkButton(app, text="SELECT DIRECTORY")
+changeDirectoryButton.pack(padx=20, pady=20)
+
+slider_volume = customtkinter.CTkSlider(app, from_=0, to=1, command=pygame.mixer.music.set_volume, orientation="vertical")
+slider_volume.pack(padx=20, pady=20)
+
+info_label=customtkinter.CTkLabel(app, text="", fg_color="transparent")
+info_label.pack(padx=20, pady=20)
+
+pygame.mixer.init()
+
 def choose_directory():
     global filesDirectory
     global filesList
@@ -24,26 +34,33 @@ def choose_directory():
     filesDirectory = filedialog.askdirectory(
         title="Choose your songs folder"
     )
-    filesList = os.listdir(filesDirectory)
-    songsCount = 0
-    for path in pathlib.Path(filesDirectory).iterdir():
-        if path.is_file():
-            songsCount += 1
-    print(filesList)
+
+    secondFilesList = os.listdir(filesDirectory)
+    filesList = []
+    print(secondFilesList)
+    for file in secondFilesList:
+        print(file)
+        if file.lower().endswith((".mp3", ".wav", ".ogg")):
+            filesList += [file]
+
+    songsCount = len(filesList)
+
+    print(songsCount, filesList)
 
 def play_music():
     if playButton.cget("text") == "STOP":
         pygame.mixer.music.stop()
         playButton.configure(text="PLAY")
-    else:
-        pygame.mixer.init()
-        pygame.mixer.music.load(os.path.join(filesDirectory, filesList[random.randint(0, songsCount-1)]))
+
+    elif playButton.cget("text") == "PLAY" and songsCount > 1:
+        pygame.mixer.music.load(os.path.join(filesDirectory, filesList[random.randint(0, songsCount - 1)]))
         pygame.mixer.music.play()
+        info_label.configure(text="")
         playButton.configure(text="STOP")
+    else:
+        info_label.configure(text="You have no sound in the selected folder")
 
 playButton.configure(command=play_music)
-
-changeDirectoryButton = customtkinter.CTkButton(app, text="SELECT DIRECTORY", command=choose_directory)
-changeDirectoryButton.pack(padx=20, pady=20)
+changeDirectoryButton.configure(command=choose_directory)
 
 app.mainloop()
