d4f437186eb1a5b5c25c0c02a15bd2ac08b1a4fb
Trollmine <trolltrollmine@gmail.com>
2026-01-08 11:00:35 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,9 +6,11 @@ import pygame
 import random
 import os
 
-app = customtkinter.CTk()
+from sqlalchemy import column
+
+app = customtkinter.CTk(fg_color="#ff8838")
 app.title("MP3-SHI")
-app.geometry("500*400")
+app.geometry("960x540")
 
 filesDirectory = "" #r"E:\PythonProject\mp3-shi\Songs"
 filesList      = []
@@ -18,14 +20,24 @@ playingSong    = -1
 musicTitle = customtkinter.CTkLabel(app, text="None", font=("", 30))
 musicTitle.pack(padx=20, pady=20)
 
-playButton = customtkinter.CTkButton(app, text="PLAY")
-playButton.pack(padx=20, pady=20)
+topFrame = customtkinter.CTkFrame(app, width=960, height=540, fg_color="#fa9857")
+topFrame.pack(padx=20, pady=20)
 
-nextButton = customtkinter.CTkButton(app, text=">")
-nextButton.pack(padx=20, pady=20)
+sliderFrame = customtkinter.CTkFrame(topFrame, fg_color="#fa9857")
+sliderFrame.grid(row=0, column=1, sticky="ew")
 
-previousButton = customtkinter.CTkButton(app, text="<")
-previousButton.pack(padx=20, pady=20)
+previousButton = customtkinter.CTkButton(topFrame, text="<", width=50, height=50)
+previousButton.grid(row=1, column=0, padx=10, pady=20)
+
+playButton = customtkinter.CTkButton(topFrame, text="PLAY", width=50, height=50)
+playButton.grid(row=1, column=1, padx=0, pady=20, sticky="ew")
+
+nextButton = customtkinter.CTkButton(topFrame, text=">", width=50, height=50)
+nextButton.grid(row=1, column=2, padx=10, pady=20)
+
+slider_time = customtkinter.CTkSlider(sliderFrame, from_=0, to=1, command=pygame.mixer.music.set_pos, state="disabled")
+slider_time.set(0)
+slider_time.grid(padx=20, pady=20)
 
 changeDirectoryButton = customtkinter.CTkButton(app, text="SELECT DIRECTORY")
 changeDirectoryButton.pack(padx=20, pady=20)
@@ -33,10 +45,6 @@ changeDirectoryButton.pack(padx=20, pady=20)
 slider_volume = customtkinter.CTkSlider(app, from_=0, to=1, command=pygame.mixer.music.set_volume)
 slider_volume.set(1)
 slider_volume.pack(padx=20, pady=20)
-
-slider_time = customtkinter.CTkSlider(app, from_=0, to=1, command=pygame.mixer.music.set_pos, state="disabled")
-slider_time.set(0)
-slider_time.pack(padx=20, pady=20)
 
 info_label = customtkinter.CTkLabel(app, text="", fg_color="transparent")
 info_label.pack(padx=20, pady=20)
